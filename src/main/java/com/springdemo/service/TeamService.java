ae826baca68165fbabcf80fd5f33f0d63cd5ff9a
abdur.rahim <abdur.rahim@businessaccelerate.com.bd>
2020-11-23 21:04:14 +0600

    first commit

---
+++ src/main/java/com/springdemo/service/TeamService.java
@@ -0,0 +1,30 @@
+package com.springdemo.service;
+
+import com.springdemo.entity.Player;
+import com.springdemo.entity.Team;
+import com.springdemo.entity.Team;
+import org.hibernate.Session;
+
+import java.util.List;
+
+public interface TeamService {
+    public List<Team> getTeams();
+
+    public  void saveTeam(Team team);
+
+    public  Team  getTeamById(int id);
+
+    public  void  deleteTeam(int id );
+
+    public  void updateTeam(Team team);
+
+    public void addPlayerToTeam(int playerId , int teamId);
+
+    void removePlayer(int teamId, int playerId);
+
+
+//    public  void addPlayerToTeam(Player player);
+//
+//    public  void  removePlayerFromTeam(Player player);
+    
+}
